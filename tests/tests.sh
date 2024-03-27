4579f6e2789916316b8f6bf4f28b130aefd7769f
Roman Badmaev <barv@inbox.ru>
2024-03-27 09:45:19 +0300

    Extend tests with running unit tests and reporting stages

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bats
 
+set -e
+
 testDir=`pwd`
 cd '..'
 srcDir=`pwd`
@@ -13,14 +15,24 @@ buildsDir="$testDir/build"
 
     mkdir -p $buildDir/library
     cd $buildDir/library && rm -rf ./*
-    cmake -DCMAKE_INSTALL_PREFIX=$installPath $srcDir/library
+    echo "--------- CONFIGURE LIB ---------------"
+    cmake -DCMAKE_INSTALL_PREFIX=$installPath -DUseTests=True $srcDir/library
+    echo "--------- BUILD LIB ---------------"
     cmake --build .
+    echo "--------- UNIT TEST LIB ---------------"
+    ctest .
+    echo "--------- INSTALL LIB ---------------"
     cmake --install .
 
+    echo "--------- CLIENT ---------------"
     mkdir -p $buildDir/client
     cd $buildDir/client && rm -rf ./*
+    echo "--------- CONFIGURE CLIENT ---------------"
     cmake -DCMAKE_PREFIX_PATH=$installPath $srcDir/client
+    echo "--------- BUILD CLIENT ---------------"
     cmake --build .
+    echo "--------- UNIT TESTS CLIENT ---------------"
+    ctest .
 }
 
 @test "movable install" {
@@ -31,14 +43,24 @@ buildsDir="$testDir/build"
     mkdir -p $buildDir/library
     originalInstallPath="$buildDir/library/install"
     cd $buildDir/library && rm -rf ./*
-    cmake -DCMAKE_INSTALL_PREFIX=$originalInstallPath $srcDir/library
+    echo "--------- CONFIGURE LIB ---------------"
+    cmake -DCMAKE_INSTALL_PREFIX=$originalInstallPath -DUseTests=True $srcDir/library
+    echo "--------- BUILD LIB ---------------"
     cmake --build .
+    echo "--------- UNIT TEST LIB ---------------"
+    ctest .
+    echo "--------- INSTALL LIB ---------------"
     cmake --install .
 
     mv $originalInstallPath $movedInstallPath
 
+    echo "--------- CLIENT ---------------"
     mkdir -p $buildDir/client
     cd $buildDir/client && rm -rf ./*
+    echo "--------- CONFIGURE CLIENT ---------------"
     cmake -DCMAKE_PREFIX_PATH=$movedInstallPath $srcDir/client
+    echo "--------- BUILD CLIENT ---------------"
     cmake --build .
+    echo "--------- UNIT TESTS CLIENT ---------------"
+    ctest .
 }
